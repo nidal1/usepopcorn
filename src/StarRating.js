bbc2b02df1f1a4fde675dc51e56a559658ff11ce
Mohamed Nidal <touhami9@gmail.com>
2023-08-08 23:23:24 +0100

    improving reusability with props

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = '#f00',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetRating = null,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tmpRating, setTmpRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating && onSetRating(rating);
   }
   return (
     <div
@@ -14,6 +23,7 @@ export default function StarRating({ maxRating = 5 }) {
         alignItems: 'center',
         gap: '16px',
       }}
+      className={className}
     >
       <div
         style={{
@@ -29,22 +39,28 @@ export default function StarRating({ maxRating = 5 }) {
             onHoverIn={() => setTmpRating(i + 1)}
             onHoverOut={() => setTmpRating(0)}
             full={tmpRating ? tmpRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
         <p
           style={{
             lineHeight: 1,
             margin: 0,
+            color,
+            fontSize: `${size / 1.5}px`,
           }}
         >
-          {tmpRating || rating || ''}
+          {messages.length === maxRating
+            ? messages[tmpRating ? tmpRating - 1 : rating - 1]
+            : tmpRating || rating || ''}
         </p>
       </div>
     </div>
   );
 }
 
-function Start({ onRate, full, onHoverIn, onHoverOut }) {
+function Start({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   return (
     <span
       onClick={onRate}
@@ -52,8 +68,8 @@ function Start({ onRate, full, onHoverIn, onHoverOut }) {
       onMouseLeave={onHoverOut}
       role="button"
       style={{
-        width: '48px',
-        height: '48px',
+        width: `${size}px`,
+        height: `${size}px`,
         display: 'flex',
         cursor: 'pointer',
       }}
@@ -62,8 +78,8 @@ function Start({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -72,7 +88,7 @@ function Start({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
