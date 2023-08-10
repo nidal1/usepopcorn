ab13e7a7b77c294a814669a4a259260da3d15421
Mohamed Nidal <touhami9@gmail.com>
2023-08-10 13:28:36 +0100

    adding a loading state

--- src/App.js
+++ src/App.js
@@ -56,14 +56,18 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(function () {
     async function fetchMovie() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`
       );
 
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovie();
   }, []);
@@ -76,7 +80,7 @@ export default function App() {
         <NumResult movies={movies} />
       </NavBar>
       <Main movies={movies}>
-        <Box element={<MovieList movies={movies} />} />
+        <Box element={isLoading ? <Loader /> : <MovieList movies={movies} />} />
         <Box
           element={
             <>
@@ -89,6 +93,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading ...</p>;
 }
 
 function NavBar({ children }) {
